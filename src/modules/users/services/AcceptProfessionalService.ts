17423457b7c8d418a5a8d0614576bc4d3d937728
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2022-02-03 11:14:25 -0300

    feat: Added mail provider, template mail provider and mail for responsible creation and professional acception

--- src/modules/users/services/AcceptProfessionalService.ts
+++ src/modules/users/services/AcceptProfessionalService.ts
@@ -1,6 +1,8 @@
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/appError';
 import { inject, injectable } from 'tsyringe';
 import IProfessionalsRepository from '../repositories/IProfessionalsRepository';
+import path from 'path';
 
 interface IRequest {
   userId: string;
@@ -11,6 +13,9 @@ class AcceptProfessionalService {
   constructor(
     @inject('ProfessionalsRepository')
     private professionalsRepository: IProfessionalsRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
   ) {}
 
   public async execute({ userId }: IRequest): Promise<void> {
@@ -22,6 +27,27 @@ class AcceptProfessionalService {
     if (professional.accepted !== null) {
       throw new AppError('Usuário já foi aceito/recusado', 400);
     }
+
+    const professionalWelcome = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'professional-welcome.hbs',
+    );
+
+    await this.mailProvider.sendMail({
+      subject: 'BEM VINDO À STAGEFY!',
+      to: {
+        email: professional.user.email,
+        name: professional.artisticName,
+      },
+      templateData: {
+        file: professionalWelcome,
+        variables: {
+          name: professional.artisticName,
+        },
+      },
+    });
 
     await this.professionalsRepository.save({
       ...professional,
